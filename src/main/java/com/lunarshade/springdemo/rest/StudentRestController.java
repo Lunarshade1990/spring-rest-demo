f67d03d49e508d979a4d82f310b97fbc110c69e0
LunarShade <na.yada1990@gmail.com>
2020-05-04 00:39:27 +0300

    Initial commit

---
+++ src/main/java/com/lunarshade/springdemo/rest/StudentRestController.java
@@ -0,0 +1,39 @@
+package com.lunarshade.springdemo.rest;
+
+import com.lunarshade.springdemo.entity.Student;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+    private List<Student> students;
+
+    @PostConstruct void loadData() {
+        students = new ArrayList<>();
+        students.add(new Student("Mario", "Rossi"));
+        students.add(new Student("Poornima", "Patel"));
+        students.add(new Student("Mary", "Smith"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+
+        if ( (studentId >= students.size()) || (studentId < 0) ) {
+            throw new StudentNotFoundException("Student id is not fount - " + studentId);
+        }
+
+        return students.get(studentId);
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 30045033
+  Date: 03.05.2020
+  Time: 13:52
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+SPRING REST DEMO
+<hr>
+<a href="test/hello">Hello</a>
+<a href="api/students">Get all students</a>
+</body>
+</html>
